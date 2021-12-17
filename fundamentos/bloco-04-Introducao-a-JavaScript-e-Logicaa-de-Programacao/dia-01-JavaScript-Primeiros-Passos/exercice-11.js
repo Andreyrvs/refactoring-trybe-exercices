abda562ff6e2567ead821fbc5d4e229b662ca387
Andreyrvs <cortaesai@gmail.com>
2021-12-17 17:08:56 -0300

    Fix: Exercice 11 - remove console.log execivos & refatora codigo

--- fundamentos/bloco-04-Introducao-a-JavaScript-e-Logicaa-de-Programacao/dia-01-JavaScript-Primeiros-Passos/exercice-11.js
+++ fundamentos/bloco-04-Introducao-a-JavaScript-e-Logicaa-de-Programacao/dia-01-JavaScript-Primeiros-Passos/exercice-11.js
@@ -28,46 +28,35 @@ Dica: que tal identificar as alíquotas com variáveis de nomes explicativos?
  */
 
 const salarioBruto = 3000;
-let inss;
-let aliquota;
+let aliquotaINSS;
+let aliquotaIR;
 
 if (salarioBruto <= 1556.94) {
-  aliquota = salarioBruto / 100 * 8;
-  inss = salarioBruto - aliquota;
-  console.log(inss);
+  aliquotaINSS = salarioBruto / 100 * 8;
 } else if (salarioBruto >= 1556.95 && salarioBruto <= 2594.92) {
-  aliquota = salarioBruto / 100 * 9;
-  inss = salarioBruto - aliquota;
-  console.log(inss);
+  aliquotaINSS = salarioBruto / 100 * 9;
 } else if (salarioBruto >= 2594.93 && salarioBruto < 5189.82) {
-  aliquota = salarioBruto / 100 * 11;
-  inss = salarioBruto - aliquota;
-  console.log(inss);
+  aliquotaINSS = salarioBruto / 100 * 11;
 } else if (salarioBruto > 5189.82) {
-  aliquota = 570.88;
-  inss = salarioBruto - aliquota;
-  console.log(inss);
+  aliquotaINSS = 570.88;
 }
 
-const salarioBase = inss
-let ir;
+console.log(aliquotaINSS);
+const salarioBase = salarioBruto - aliquotaINSS;
 
 if (salarioBase <= 1903.98) {
-  ir = 'isento de imposto de renda'
-  console.log(ir);
+  aliquotaIR = 'isento de imposto de renda'
 } else if (salarioBase >= 1903.99 && salarioBase <= 2826.655) {
-  ir = (salarioBase / 100 * 7.5) - 142.80;
-  console.log(ir.toFixed(2));
+  aliquotaIR = (salarioBase / 100 * 7.5) - 142.80;
 } else if (salarioBase >= 2826.66 && salarioBase <= 3751.05) {
-  ir = (salarioBase / 100 * 15) - 354.80;
-  console.log(ir.toFixed(2));
+  aliquotaIR = (salarioBase / 100 * 15) - 354.80;
 } else if (salarioBase >= 3751.06 && salarioBase <= 4664.68) {
-  ir = (salarioBase / 100 * 22.5) - 636.13;
-  console.log(ir.toFixed(2));
+  aliquotaIR = (salarioBase / 100 * 22.5) - 636.13;
 } else if (salarioBase > 4664.68) {
-  ir = (salarioBase / 100 * 27.5) - 869.36;
-  console.log(ir.toFixed(2));
+  aliquotaIR = (salarioBase / 100 * 27.5) - 869.36;
 }  
 
-let salarioLiquido = salarioBase - ir;
+console.log(aliquotaIR.toFixed(2));
+
+let salarioLiquido = salarioBase - aliquotaIR;
 console.log(`Salário líquido: ${salarioLiquido}`);
