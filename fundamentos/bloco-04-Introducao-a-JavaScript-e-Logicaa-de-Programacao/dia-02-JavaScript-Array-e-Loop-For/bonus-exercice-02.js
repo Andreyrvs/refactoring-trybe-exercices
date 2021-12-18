904d4f1b0def73e80e4612a2257c0ee17ce3da29
Andreyrvs <cortaesai@gmail.com>
2021-12-18 15:23:53 -0300

    Feat: bonus exercice-02

--- fundamentos/bloco-04-Introducao-a-JavaScript-e-Logicaa-de-Programacao/dia-02-JavaScript-Array-e-Loop-For/bonus-exercice-02.js
+++ fundamentos/bloco-04-Introducao-a-JavaScript-e-Logicaa-de-Programacao/dia-02-JavaScript-Array-e-Loop-For/bonus-exercice-02.js
@@ -2,7 +2,8 @@ array = [45, 27, 57, 1330, 560, 800, 200, 70, 945, 54];
 
 for (let index = 1; index < array.length; index += 1) {
   for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
-    if (array[index] < array[secondIndex]) {
+    // alterado sinal de "menor" para "maior". Assim ordenando em forma Decrescente!
+    if (array[index] > array[secondIndex]) {
       let position = array[index];
       array[index] = array[secondIndex];
       array[secondIndex] = position;
