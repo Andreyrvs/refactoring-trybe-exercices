42aeb47656c9eef5300508f8511e51f28c2f52d1
Andreyrvs <cortaesai@gmail.com>
2021-12-18 12:50:09 -0300

    Fix: Eslint exercice 02, 03

--- fundamentos/bloco-04-Introducao-a-JavaScript-e-Logicaa-de-Programacao/dia-01-JavaScript-Primeiros-Passos/exercice-02.js
+++ fundamentos/bloco-04-Introducao-a-JavaScript-e-Logicaa-de-Programacao/dia-01-JavaScript-Primeiros-Passos/exercice-02.js
@@ -2,6 +2,7 @@ const a = 2;
 const b = 4;
 
 if (a > b) {
-  console.log("'a' é maior que 'b'");
-} else
-console.log("'b' é maior que 'a'");
+  console.log('"a" é maior que "b"');
+} else {
+  console.log('"b" é maior que "a');
+}
