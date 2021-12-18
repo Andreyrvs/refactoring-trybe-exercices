57f71d35d353a2eb27afc0cdbb2d431883f75f8c
Andreyrvs <cortaesai@gmail.com>
2021-12-18 12:46:02 -0300

    Fix: Eslint exercice-01

--- fundamentos/bloco-04-Introducao-a-JavaScript-e-Logicaa-de-Programacao/dia-01-JavaScript-Primeiros-Passos/exercice-01.js
+++ fundamentos/bloco-04-Introducao-a-JavaScript-e-Logicaa-de-Programacao/dia-01-JavaScript-Primeiros-Passos/exercice-01.js
@@ -1,8 +1,8 @@
 const a = 3;
 const b = 2;
 
-console.log('Soma: ' + (a + b));
-console.log('Subtração: '+ (a - b));
-console.log('Multiplicação: ' + (a * b));
-console.log('Divisão: ' + (a / b));
-console.log('Módulo: ' + (a % b));
+console.log(`Soma: ${a + b}`);
+console.log(`Subtração: ${a - b}`);
+console.log(`Multiplicação: ${a * b}`);
+console.log(`Divisão: ${a / b}`);
+console.log(`Módulo: ${a % b}`);
