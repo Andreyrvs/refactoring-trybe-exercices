a7d8ea91b5e53eee9ec1cb0a6529c330acf8249f
Andreyrvs <cortaesai@gmail.com>
2021-12-18 13:13:35 -0300

    Fix: Eslint - para-fixar, exercice-11

--- fundamentos/bloco-04-Introducao-a-JavaScript-e-Logicaa-de-Programacao/dia-01-JavaScript-Primeiros-Passos/para-fixar/03-operadores-logicos.js
+++ fundamentos/bloco-04-Introducao-a-JavaScript-e-Logicaa-de-Programacao/dia-01-JavaScript-Primeiros-Passos/para-fixar/03-operadores-logicos.js
@@ -1,33 +1,32 @@
 // Operador AND
 
 const currentHour = 6;
-let message = '';
+let message;
 
 if (currentHour >= 22) {
-  message = 'Não deveríamos comer nada, é hora de dormir'
+  message = 'Não deveríamos comer nada, é hora de dormir';
 } else if (currentHour >= 18 && currentHour < 22) {
-  message = 'Rango da noite, vamos jantar :D'
+  message = 'Rango da noite, vamos jantar :D';
 } else if (currentHour >= 14 && currentHour < 18) {
-  message = 'Vamos fazer um bolo pro café da tarde?'
+  message = 'Vamos fazer um bolo pro café da tarde?';
 } else if (currentHour > 11 && currentHour < 14) {
-  message = 'Hora do almoço!!!'
+  message = 'Hora do almoço!!!';
 } else if (currentHour > 4 && currentHour < 11) {
-  message = 'Hmmm, cheiro de café recém passado'
+  message = 'Hmmm, cheiro de café recém passado';
 }
 
-// console.log(message);
+console.log(message);
 
 // Operador OR
 
-let weekDay =  'domingo';
+const weekDay = 'domingo';
 
 if (weekDay === 'segunda-feira'
     || weekDay === 'terça-feira' 
     || weekDay === 'quarta-feira'
     || weekDay === 'quinta-feira'
-    || weekDay === 'sexta-feira' ) {
+    || weekDay === 'sexta-feira') {
   console.log('Oba, mais um dia de aprendizado na Trybe >:D');
-
 } else {
   console.log('FINALMENTE, descanso merecido UwU');
 }
