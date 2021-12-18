932e557b5aee48e20aceb52d49b05beebaae8da5
Andreyrvs <cortaesai@gmail.com>
2021-12-18 13:53:15 -0300

    Feat: para-fixar - for

--- fundamentos/bloco-04-Introducao-a-JavaScript-e-Logicaa-de-Programacao/dia-02-JavaScript-Array-e-Loop-For/para-fixa/array.js
+++ fundamentos/bloco-04-Introducao-a-JavaScript-e-Logicaa-de-Programacao/dia-02-JavaScript-Array-e-Loop-For/para-fixa/array.js
@@ -1,5 +1,4 @@
 // Exercício 1
-// Obtenha o valor "Serviços" do array menu :
 
 let menu1 = ['Home', 'Serviços', 'Portfólio', 'Links'];
 let menuServices = menu1[1];
@@ -7,7 +6,6 @@ let menuServices = menu1[1];
 console.log(menuServices);
 
 // Exercício 2
-// Procure o índice do valor "Portfólio" do array menu :
 
 let menu2 = ['Home', 'Serviços', 'Portfólio', 'Links'];
 let indexOfPortfolio = menu2[2];
@@ -15,10 +13,8 @@ let indexOfPortfolio = menu2[2];
 console.log(indexOfPortfolio);
 
 // Exercício 3
-// Adicione o valor "Contato" no final do array menu :
 
 let menu3 = ['Home', 'Serviços', 'Portfólio', 'Links'];
 
 menu3.push('Contato');
-
 console.log(menu3);
