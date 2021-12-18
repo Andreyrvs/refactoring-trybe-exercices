71ef4e5f6d8f692803134ac23b4a8dcb772b4c6b
Andreyrvs <cortaesai@gmail.com>
2021-12-18 12:54:04 -0300

    Fix: Eslint - exercice - 04 ate 09

--- fundamentos/bloco-04-Introducao-a-JavaScript-e-Logicaa-de-Programacao/dia-01-JavaScript-Primeiros-Passos/exercice-04.js
+++ fundamentos/bloco-04-Introducao-a-JavaScript-e-Logicaa-de-Programacao/dia-01-JavaScript-Primeiros-Passos/exercice-04.js
@@ -2,7 +2,7 @@ const numero = -10;
 
 if (numero > 0) {
   console.log('positive');
-} else if (numero <  0) {
+} else if (numero < 0) {
   console.log('negative');
 } else {
   console.log('zero');
--- fundamentos/bloco-04-Introducao-a-JavaScript-e-Logicaa-de-Programacao/dia-01-JavaScript-Primeiros-Passos/exercice-08.js
+++ fundamentos/bloco-04-Introducao-a-JavaScript-e-Logicaa-de-Programacao/dia-01-JavaScript-Primeiros-Passos/exercice-08.js
@@ -2,9 +2,9 @@ const numberOne = 92;
 const numberTwo = 5;
 const numberThree = 7;
 
-let isEven = false
+let isEven = false;
 
 if (numberOne % 2 === 0 || numberTwo % 2 === 0 || numberThree % 2 === 0) {
-  isEven = true
+  isEven = true;
 } 
 console.log(isEven);
