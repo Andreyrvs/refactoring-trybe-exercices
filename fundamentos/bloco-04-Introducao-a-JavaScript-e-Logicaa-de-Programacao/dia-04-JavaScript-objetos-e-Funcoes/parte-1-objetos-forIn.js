4b96e44135dcc0aef1a2301ba71371e2f5ad7eb9
Andreyrvs <cortaesai@gmail.com>
2021-12-19 14:54:11 -0300

    Feat: Parte 1 - exercicio 05, auxilio gabarito

--- fundamentos/bloco-04-Introducao-a-JavaScript-e-Logicaa-de-Programacao/dia-04-JavaScript-objetos-e-Funcoes/parte-1-objetos-forIn.js
+++ fundamentos/bloco-04-Introducao-a-JavaScript-e-Logicaa-de-Programacao/dia-04-JavaScript-objetos-e-Funcoes/parte-1-objetos-forIn.js
@@ -5,8 +5,8 @@ const info = {
 };
 
 // ========= Exercice 01 ========= //
-const grettings = `Bem-vinda, ${info.personagem}`
-// console.log(grettings);
+const grettings = `Bem-vinda, ${info.personagem}`;
+console.log(grettings);
 
 // ========= Exercice 02 ========= //
 info.recorrente = 'Sim';
@@ -14,10 +14,34 @@ info.recorrente = 'Sim';
 
 // ========= Exercice 03 ========= //
 for (const key in info) {
-  // console.log(key);
+  if (info) {
+    console.log(key);
+  }
 }
 
 // ========= Exercice 04 ========= //
 for (const key in info) {
-  console.log(info[key]);
+  if (info) {
+    console.log(info[key]);
+  }
+}
+
+// ========= Exercice 05 ========= //
+const newInfo = {
+  personagem: 'Tio Patinhas',
+  origem: 'Christmas on Bear Mountain, Dells Four Color Comics #178',
+  nota: 'O último MacPatinhas',
+  recorrente: 'Sim',
+};
+
+for (const key in info) {
+  if (
+    key === 'recorrente' 
+    && info[key] === 'Sim' 
+    && newInfo[key] === 'Sim'
+  ) {
+    console.log('Ambos recorrentes');
+  } else {
+    console.log(`${info[key]} e ${newInfo[key]}`);
+  }
 }
