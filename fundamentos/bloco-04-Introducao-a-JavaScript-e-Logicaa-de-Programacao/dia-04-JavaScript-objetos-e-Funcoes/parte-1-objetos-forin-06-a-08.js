2c3fdeb37ef8a28bfb8673991501519ed7febb8b
Andreyrvs <cortaesai@gmail.com>
2021-12-19 15:00:16 -0300

    Feat: Parte 1 - exercicio 07

--- fundamentos/bloco-04-Introducao-a-JavaScript-e-Logicaa-de-Programacao/dia-04-JavaScript-objetos-e-Funcoes/parte-1-objetos-forin-06-a-08.js
+++ fundamentos/bloco-04-Introducao-a-JavaScript-e-Logicaa-de-Programacao/dia-04-JavaScript-objetos-e-Funcoes/parte-1-objetos-forin-06-a-08.js
@@ -10,8 +10,21 @@ const leitor = {
     },
   ],
 };
-/* 6 - Acesse as chaves nome , sobrenome e titulo , que está dentro da chave livrosFavoritos , e faça um console.log no seguinte formato: "O livro favorito de Julia Pessoa se chama 'O Pior Dia de Todos'". */
+
+// ========= Exercice 06 ========= //
 const favoriteBook = `O livro favorito de ${leitor.nome} ${leitor.sobrenome} 
 se chama ${leitor.livrosFavoritos[0].titulo} `;
 
-console.log(favoriteBook);
+// console.log(favoriteBook);
+
+// ========= Exercice 07 ========= //
+
+leitor.livrosFavoritos[1] = {
+  titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+  autor: 'JK Rowling',
+  editor: 'Rocco',
+} 
+
+console.log(leitor);
+
+// ========= Exercice 08 ========= //
