242d498138220723ba36b1cd20c81289cad1c5d3
Andreyrvs <cortaesai@gmail.com>
2021-12-18 23:50:50 -0300

    Feat: bonus exercice 01

---
+++ fundamentos/bloco-04-Introducao-a-JavaScript-e-Logicaa-de-Programacao/dia-03-JavaScript-Logica-de-programacao-e-Algoritimos/bonus-exercice.js
@@ -0,0 +1,10 @@
+const n = 5;
+const asterisco = '*';
+let inputLine = '';
+
+for (let linha = 0; linha < n; linha += 1) {
+  inputLine += asterisco;
+}
+for (let linha = 0; linha < n; linha += 1) {
+  console.log(inputLine);
+}     
