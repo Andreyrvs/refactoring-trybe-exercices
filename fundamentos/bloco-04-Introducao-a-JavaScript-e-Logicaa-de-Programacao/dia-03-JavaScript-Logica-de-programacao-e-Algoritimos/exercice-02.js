fa2cb26f618c109b8fffc035a9ed060561c50727
Andreyrvs <cortaesai@gmail.com>
2021-12-18 23:06:20 -0300

    Feat: exercice 02

---
+++ fundamentos/bloco-04-Introducao-a-JavaScript-e-Logicaa-de-Programacao/dia-03-JavaScript-Logica-de-programacao-e-Algoritimos/exercice-02.js
@@ -0,0 +1,7 @@
+const word = 'triber';
+let reverseWord = '';
+
+for (let index = 0; index < word.length; index += 1) {
+  reverseWord += word[word.length - 1 - index];
+}
+console.log(reverseWord);
