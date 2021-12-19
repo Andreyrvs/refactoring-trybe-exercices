c3cc642af0c7c3fd87b577aa0377a6df12c02100
Andreyrvs <cortaesai@gmail.com>
2021-12-19 19:06:53 -0300

    Feat: Parte 2 - exercicio 06

--- fundamentos/bloco-04-Introducao-a-JavaScript-e-Logicaa-de-Programacao/dia-01-JavaScript-Primeiros-Passos/exercice-09.js
+++ fundamentos/bloco-04-Introducao-a-JavaScript-e-Logicaa-de-Programacao/dia-01-JavaScript-Primeiros-Passos/exercice-09.js
@@ -1,7 +1,3 @@
-/* 
-Escreva um programa que defina três números em constantes e retorne true se pelo menos uma das três for ímpar. Caso contrário, ele retorna false .
-Bonus: use somente um if . */
-
 const numberOne = 92;
 const numberTwo = 52;
 const numberThree = 72;
---
+++ fundamentos/bloco-04-Introducao-a-JavaScript-e-Logicaa-de-Programacao/dia-04-JavaScript-objetos-e-Funcoes/parte-2-funcoes-06.js
@@ -0,0 +1,8 @@
+function somatorio(number) {
+  let total = 0;
+  for (let index = 1; index <= number; index += 1) {
+    total += index
+  }
+  return total
+}
+console.log(somatorio(5));
