c6349e56b98f4c99e9c01a331e15a8aa180bee30
Andreyrvs <cortaesai@gmail.com>
2021-12-19 00:15:02 -0300

    Feat: bonus exercice 05 - auxilio gabarito

---
+++ fundamentos/bloco-04-Introducao-a-JavaScript-e-Logicaa-de-Programacao/dia-03-JavaScript-Logica-de-programacao-e-Algoritimos/bonus-exercice-05.js
@@ -0,0 +1,18 @@
+const n = 7;
+const middle = (n + 1) / 2;
+let controlLeft = middle;
+let controlRight = middle;
+const symbol = '*';
+for (let line = 1; line <= middle; line += 1) {
+  let outputLine = '';
+  for (let col = 1; col <= n; col += 1) {
+    if (col === controlLeft || col === controlRight || line === middle) {
+      outputLine += symbol;
+    } else {
+      outputLine += ' ';
+    }
+  }
+  controlLeft -= 1;
+  controlRight += 1;
+  console.log(outputLine);
+}
