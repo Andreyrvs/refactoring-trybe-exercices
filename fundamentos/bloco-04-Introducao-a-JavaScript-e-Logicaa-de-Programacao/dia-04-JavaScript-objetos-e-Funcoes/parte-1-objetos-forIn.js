c6aeccbe35ec48b125a637d72c37eb158d3e32da
Andreyrvs <cortaesai@gmail.com>
2021-12-19 14:33:43 -0300

    Feat: Parte 1 - exercicio 01

---
+++ fundamentos/bloco-04-Introducao-a-JavaScript-e-Logicaa-de-Programacao/dia-04-JavaScript-objetos-e-Funcoes/parte-1-objetos-forIn.js
@@ -0,0 +1,13 @@
+const info = {
+  personagem: 'Margarida',
+  origem: 'Pato Donald',
+  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+};
+
+
+/* 1 - Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome. Valor esperado no console:
+Copiar
+Bem-vinda, Margarida */
+// ========= Exercice 01 ========= //
+const grettings = `Bem-vinda, ${info.personagem}`
+console.log(grettings);
