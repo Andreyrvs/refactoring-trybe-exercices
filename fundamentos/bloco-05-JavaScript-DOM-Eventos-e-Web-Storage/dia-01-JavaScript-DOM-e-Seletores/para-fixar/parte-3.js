6f5721271ad670be0fc4e2f36b3e21c9c3d5ce4b
Andreyrvs <cortaesai@gmail.com>
2021-12-21 21:38:30 -0300

    Feat: para-fixar parte-3

---
+++ fundamentos/bloco-05-JavaScript-DOM-Eventos-e-Web-Storage/dia-01-JavaScript-DOM-e-Seletores/para-fixar/parte-3.js
@@ -0,0 +1,19 @@
+const uI = document.getElementsByTagName('h3')[0];
+uI.style.backgroundColor = '#A500F3';
+
+const uNI = document.getElementsByTagName('h3')[1];
+uNI.style.backgroundColor = '#A500F3';
+
+const nUI = document.getElementsByTagName('h3')[2];
+nUI.style.backgroundColor = '#232525';
+
+const nUNI = document.getElementsByTagName('h3')[3];
+nUNI.style.backgroundColor = '#232525';
+nUNI.style.color = 'white'
+
+const emergencyT =  document.querySelector('.emergency-tasks');
+emergencyT.style.backgroundColor = '#FF9F84';
+
+const noEmergencyT =  document.querySelector('.no-emergency-tasks');
+noEmergencyT.style.backgroundColor = '#F9DB5E';
+
