e2254d4af0d86f8b145392e01eb36261a6d30b4c
Andreyrvs <cortaesai@gmail.com>
2021-12-23 17:41:52 -0300

    Fix: Para Fixar - addEventListener 02

--- fundamentos/bloco-05-JavaScript-DOM-Eventos-e-Web-Storage/dia-03-JavaScript-eventos/para-fixar/main.js
+++ fundamentos/bloco-05-JavaScript-DOM-Eventos-e-Web-Storage/dia-03-JavaScript-eventos/para-fixar/main.js
@@ -4,17 +4,16 @@ const thirdLi = document.getElementById('third-li');
 const input = document.getElementById('input');
 const myWebpage = document.getElementById('my-spotrybefy');
 
-
 // 1. Copie esse arquivo e edite apenas ele;
 // 1.1. Antes de começar os exercícios, use o LiveServer para dar uma olhada em como está a página no navegador.
 // 1.2. Note que uma das caixas está um pouco acima das outras. Por que isso ocorre?
 
 // // 2. Crie uma função que adicione a classe 'tech' ao elemento `li` quando for clicado.
 // 2.1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
-function changeTech() {
+function changeTech(event) {
   const techElement = document.querySelector('.tech');
-  techElement.remove('tech');
-  techElement.add('tech');
+  techElement.classList.remove('tech');
+  event.target.classList.add('tech');
   input.value = '';
 }
 
@@ -27,20 +26,26 @@ thirdLi.addEventListener('click', changeTech);
 input.addEventListener('input', (evento) => {
   const techClass = document.querySelector('.tech');
   techClass.innerText = evento.target.value;
-})
+});
 
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 // redirecione para alguma página;
 // 4.1. Que tal redirecionar para seu portifólio?
 myWebpage.addEventListener('dblclick', () => {
-  window.location.replace('https://blog.betrybe.com/')
-})
+  window.location.replace('https://blog.betrybe.com/');
+});
 
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo;
+myWebpage.addEventListener('mouseover', (event) => {
+  event.target.style.color = 'lime';
+});
+
+myWebpage.addEventListener('mouseout', (event) => {
+  event.target.style.color = 'unset'
+})
 
 // Segue abaixo um exemplo do uso de event.target:
-
 
 function resetText(event) {
   // O Event é passado como um parâmetro para a função.
