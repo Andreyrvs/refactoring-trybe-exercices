43947cb622708b22964305561fe7629604ca3ac5
Andreyrvs <cortaesai@gmail.com>
2021-12-23 10:47:49 -0300

    Feat: Para Fixar - criando elementos 02

--- fundamentos/bloco-05-JavaScript-DOM-Eventos-e-Web-Storage/dia-02-JavaScript-Trabalhando-Com-Elementos/para-fixar/criando-elementos.js
+++ fundamentos/bloco-05-JavaScript-DOM-Eventos-e-Web-Storage/dia-02-JavaScript-Trabalhando-Com-Elementos/para-fixar/criando-elementos.js
@@ -1,3 +1,7 @@
 const irmao = document.createElement('section');
 irmao.id = 'irmaoCriado'
 pai.appendChild(irmao)
+
+const filhaCriado = document.createElement('section');
+filhaCriado.id = 'filhaCriada'
+estouAqui.appendChild(filhaCriado)
