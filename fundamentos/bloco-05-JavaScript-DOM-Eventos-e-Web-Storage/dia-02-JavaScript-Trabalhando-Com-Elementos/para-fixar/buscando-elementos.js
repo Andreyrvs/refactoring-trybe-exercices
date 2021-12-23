eee9a80ef589881b5b7b8fbcdaa6ea9bfd76abb8
Andreyrvs <cortaesai@gmail.com>
2021-12-23 10:40:41 -0300

    Feat: Para Fixar - buscando elementos 08

--- fundamentos/bloco-05-JavaScript-DOM-Eventos-e-Web-Storage/dia-02-JavaScript-Trabalhando-Com-Elementos/para-fixar/buscando-elementos.js
+++ fundamentos/bloco-05-JavaScript-DOM-Eventos-e-Web-Storage/dia-02-JavaScript-Trabalhando-Com-Elementos/para-fixar/buscando-elementos.js
@@ -17,9 +17,10 @@ const pai = document.querySelector('#pai');
 // console.log(estouAqui.parentElement.firstElementChild);
 
 // ===== Exercico 06 ===== //
-console.log(estouAqui.nextSibling);
+// console.log(estouAqui.nextSibling);
 
 // ===== Exercico 07 ===== //
-console.log(estouAqui.nextElementSibling);
+// console.log(estouAqui.nextElementSibling);
 
 // ===== Exercico 08 ===== //
+console.log(pai.children[2]);
