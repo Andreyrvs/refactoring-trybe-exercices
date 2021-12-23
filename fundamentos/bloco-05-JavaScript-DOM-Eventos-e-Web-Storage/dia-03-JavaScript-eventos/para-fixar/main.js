9b224a833ed3f3adddddc381d4a3e609e4f877b9
Andreyrvs <cortaesai@gmail.com>
2021-12-23 17:31:53 -0300

    Feat: Para Fixar - addEventListener 04

--- fundamentos/bloco-05-JavaScript-DOM-Eventos-e-Web-Storage/dia-03-JavaScript-eventos/para-fixar/main.js
+++ fundamentos/bloco-05-JavaScript-DOM-Eventos-e-Web-Storage/dia-03-JavaScript-eventos/para-fixar/main.js
@@ -32,6 +32,9 @@ input.addEventListener('input', (evento) => {
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 // redirecione para alguma página;
 // 4.1. Que tal redirecionar para seu portifólio?
+myWebpage.addEventListener('dblclick', () => {
+  window.location.replace('https://blog.betrybe.com/')
+})
 
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo;
