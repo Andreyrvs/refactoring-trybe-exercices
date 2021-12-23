953ba3f8005b86d192943fd0fdc8350a1a0feb93
Andreyrvs <cortaesai@gmail.com>
2021-12-23 11:44:48 -0300

    Feat: Exercice 02

--- fundamentos/bloco-05-JavaScript-DOM-Eventos-e-Web-Storage/dia-02-JavaScript-Trabalhando-Com-Elementos/script.js
+++ fundamentos/bloco-05-JavaScript-DOM-Eventos-e-Web-Storage/dia-02-JavaScript-Trabalhando-Com-Elementos/script.js
@@ -1,5 +1,11 @@
+// ===== Exercice 01 ===== //
 const body = document.querySelector('body');
 const h1 = document.createElement('h1');
 h1.innerHTML = 'sou h1'
 body.appendChild(h1)
 console.log(body);
+
+// ===== Exercice 02 ===== //
+const main = document.createElement('main');
+main.className = 'main-content';
+body.appendChild(main);
