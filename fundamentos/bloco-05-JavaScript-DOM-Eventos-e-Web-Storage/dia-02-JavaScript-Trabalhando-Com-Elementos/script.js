daf2acfa47159c5c490a159711556ba4f8b030a2
Andreyrvs <cortaesai@gmail.com>
2021-12-23 11:56:06 -0300

    Feat: Exercice 06

--- fundamentos/bloco-05-JavaScript-DOM-Eventos-e-Web-Storage/dia-02-JavaScript-Trabalhando-Com-Elementos/script.js
+++ fundamentos/bloco-05-JavaScript-DOM-Eventos-e-Web-Storage/dia-02-JavaScript-Trabalhando-Com-Elementos/script.js
@@ -21,7 +21,15 @@ centerContent.appendChild(tagP);
 
 // ===== Exercice 05 ===== //
 const leftContent = document.createElement('section');
+leftContent.className = 'left-content'
 leftContent.innerHTML = 'Esquerda'
 mainContent.appendChild(leftContent);
 
 // ===== Exercice 06 ===== //
+const rightContent = document.createElement('section');
+rightContent.className = 'right-content';
+rightContent.innerHTML = 'Direita';
+mainContent.appendChild(rightContent);
+
+// ===== Exercice 06 ===== //
+
