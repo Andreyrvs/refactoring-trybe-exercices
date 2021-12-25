4d52b7d399e694aec85ce0372583db2f40ecf38a
Andreyrvs <cortaesai@gmail.com>
2021-12-24 21:37:27 -0300

    Feat: exercice 01 bgColor

--- fundamentos/bloco-05-JavaScript-DOM-Eventos-e-Web-Storage/dia-04-JavaScript-Web-Storage/script.js
+++ fundamentos/bloco-05-JavaScript-DOM-Eventos-e-Web-Storage/dia-04-JavaScript-Web-Storage/script.js
@@ -1,9 +1,11 @@
+// ========= Exercice 01 ========= //
 function setBackgroundColor(color) {
   const content = document.querySelector('.content');
   content.style.backgroundColor = color;
   localStorage.setItem('backgroundColor', color);
 }
 
+// Background Color
 const bgColorBtns = document.querySelectorAll('#background-color>button');
 for (let index = 0; index < bgColorBtns.length; index += 1) {
   bgColorBtns[index].addEventListener('click', (event) => {
@@ -11,12 +13,34 @@ for (let index = 0; index < bgColorBtns.length; index += 1) {
   });
 }
 
+// ========= Exercice 02 ========= //
+function setFontColor(color) {
+  const paragraphs = document.querySelectorAll('.paragraph');
+  for (let index = 0; index < paragraphs.length; index += 1) {
+    paragraphs[index].style.color = color;
+  }
+  localStorage.setItem('fontColor', color);
+}
+
+const fontColorBtns = document.querySelectorAll('#font-color>button')
+for (let index = 0; index < fontColorBtns.length; index += 1) {
+  fontColorBtns[index].addEventListener('click', (event) => {
+    setFontColor(event.target.innerHTML);
+  })
+}
+
+// ========= Exercice 03 ========= //
+
+
 function initialize() {
   const backgroundColor = localStorage.getItem('backgroundColor');
-  if (backgroundColor) setBackgroundColor(backgroundColor);
+  if (backgroundColor) return setBackgroundColor(backgroundColor);
+
+  const fontColor = localStorage.getItem('fontColor');
+  if (fontColor) return setFontColor(fontColor);
 }
-initialize();
 
 window.onload = () => {
   setBackgroundColor();
+  initialize();
 };
