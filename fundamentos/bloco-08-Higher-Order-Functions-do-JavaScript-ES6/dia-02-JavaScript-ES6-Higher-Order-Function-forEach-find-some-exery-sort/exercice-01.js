e854fe8377153be08eefcafe76aaca3d92034cd2
Andreyrvs <cortaesai@gmail.com>
2021-12-30 13:12:08 -0300

    Feat: eslint exerice 01 && 02

--- fundamentos/bloco-08-Higher-Order-Functions-do-JavaScript-ES6/dia-02-JavaScript-ES6-Higher-Order-Function-forEach-find-some-exery-sort/exercice-01.js
+++ fundamentos/bloco-08-Higher-Order-Functions-do-JavaScript-ES6/dia-02-JavaScript-ES6-Higher-Order-Function-forEach-find-some-exery-sort/exercice-01.js
@@ -1,7 +1,7 @@
 const { books } = require('./data');
 
-// 1 - Encontre o nome da primeira pessoa autora do livro nascida no ano de 1947.
-// Dica: use a função find .
+// ========= 1 - Encontre o nome da primeira pessoa autora do livro nascida no ano de 1947. ========= //
+// ========= Dica: use a função find . ========= //
 function authorBornIn1947() {
   // escreva aqui o seu código
   return books.find((book) => book.author.birthYear === 1947).author.name;
