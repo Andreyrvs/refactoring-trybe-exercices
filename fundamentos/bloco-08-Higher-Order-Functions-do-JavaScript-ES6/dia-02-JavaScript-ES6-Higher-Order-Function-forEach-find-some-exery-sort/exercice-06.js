821035b7959829c732c3c19211a6607d49eb3dd0
Andreyrvs <cortaesai@gmail.com>
2021-12-30 13:39:03 -0300

    Feat: exercice 06

---
+++ fundamentos/bloco-08-Higher-Order-Functions-do-JavaScript-ES6/dia-02-JavaScript-ES6-Higher-Order-Function-forEach-find-some-exery-sort/exercice-06.js
@@ -0,0 +1,12 @@
+const assert = require('assert');
+const { books } = require('./data');
+
+// ========== 6 - Faça uma função que retorne true , se algum livro foi lançado na década de 80, e false , caso contrário. ========== //
+
+const expectedResult = true;
+
+function someBookWasReleaseOnThe80s() {
+  return books.some((book) => book.releaseYear > 1980 && book.releaseYear < 1989);
+}
+
+assert.deepStrictEqual(someBookWasReleaseOnThe80s(), expectedResult);
