654ba4e6622447ebf6775ba75d17919f0c7241df
Andreyrvs <cortaesai@gmail.com>
2021-12-30 13:10:12 -0300

    Feat: exercice 01 && 02

--- fundamentos/bloco-08-Higher-Order-Functions-do-JavaScript-ES6/dia-02-JavaScript-ES6-Higher-Order-Function-forEach-find-some-exery-sort/para-fixar/find.js
+++ fundamentos/bloco-08-Higher-Order-Functions-do-JavaScript-ES6/dia-02-JavaScript-ES6-Higher-Order-Function-forEach-find-some-exery-sort/para-fixar/find.js
@@ -1,20 +1,14 @@
 // ========== 1 - Utilize o find para retornar o primeiro número do array que é divisível por 3 e 5 , caso ele exista: ========== //
 const numbers = [19, 21, 30, 3, 45, 22, 15];
 
-const findDivisibleBy3And5 = () => {
-  // Adiciona seu código aqui
-  return numbers.find((number) => number % 3 === 0 || number % 5 === 0);
-};
+const findDivisibleBy3And5 = () => numbers.find((number) => number % 3 === 0 || number % 5 === 0);
 
 console.log(findDivisibleBy3And5());
 
 // ========== 2 - Utilize o find para encontrar o primeiro nome com cinco letras, caso ele exista: ========== //
 const names = ['João', 'Irene', 'Fernando', 'Maria'];
 
-const findNameWithFiveLetters = () => {
-  // Adicione seu código aqui:
-  return names.find((name) => name.length === 5);
-};
+const findNameWithFiveLetters = () => names.find((name) => name.length === 5);
 
 console.log(findNameWithFiveLetters());
 
@@ -26,7 +20,6 @@ const musicas = [
 ];
 
 function findMusic(id) {
-  // Adicione seu código aqui
   return musicas.find((musica) => musica.id === id);
 }
 
